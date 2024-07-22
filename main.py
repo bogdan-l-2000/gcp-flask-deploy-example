5bb3f2f2862a9fb3f90b253cf3b505ccd94562c2
Bogdan Lyashenko <blyashenko0@gmail.com>
2024-07-22 20:22:01 +0000

    Added comments to main.py file from example

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from flask import Flask
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
     return "Hello World!"
 
 @app.route('/echo/<name>')
 def echo(name):
+    print(f"This was placed in the url: new-{name}")
     val = {"new-name": name}
     return jsonify(val)
 
